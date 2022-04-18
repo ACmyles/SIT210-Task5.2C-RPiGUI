40803cbf6e439ad77314a2e834185530bda22aa3
ACmyles <77598844+ACmyles@users.noreply.github.com>
2022-04-18 17:27:48 +1000

    refactor to radio buttons

--- RPiGUI.py
+++ RPiGUI.py
@@ -5,24 +5,45 @@ import tkinter
 gui = tkinter.Tk()
 gui.title("RPiGUI LED Control")
 
+state = tkinter.IntVar()
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(19, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 
+GPIO.output(19, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
+GPIO.output(23, GPIO.LOW)
+
 
 def toggle(led):
-    if GPIO.input(led):
-        GPIO.output(led,GPIO.LOW)
-    else:
-        GPIO.output(led,GPIO.HIGH)
+    if (led == 19):
+        GPIO.output(led, GPIO.HIGH)
+        GPIO.output(21, GPIO.LOW)
+        GPIO.output(23, GPIO.LOW)
+    elif (led == 21):
+        GPIO.output(led, GPIO.HIGH)
+        GPIO.output(19, GPIO.LOW)
+        GPIO.output(23, GPIO.LOW)
+    elif (led == 23):
+        GPIO.output(led, GPIO.HIGH)
+        GPIO.output(19, GPIO.LOW)
+        GPIO.output(21, GPIO.LOW)
 
-gui_blue_button = tkinter.Button(gui,text="Toggle Blue LED",command=lambda: toggle(21),width=15)
-gui_green_button = tkinter.Button(gui,text="Toggle Green LED",command=lambda: toggle(19),width=15)
-gui_yellow_button = tkinter.Button(gui,text="Toggle Yellow LED",command=lambda: toggle(23),width=15)
 
-gui_blue_button.pack()
+gui_red_button = tkinter.Radiobutton(
+    gui, text="Red", variable=state, value=23, command=lambda: toggle(23), width=15)
+gui_green_button = tkinter.Radiobutton(
+    gui, text="Green", variable=state, value=19, command=lambda: toggle(19), width=15)
+gui_blue_button = tkinter.Radiobutton(
+    gui, text="Blue", variable=state, value=21, command=lambda: toggle(21), width=15)
+exit_button = tkinter.Button(gui, text="Exit", command=gui.destroy, width=15)
+
+gui_red_button.pack()
 gui_green_button.pack()
-gui_yellow_button.pack()
+gui_blue_button.pack()
+exit_button.pack()
 
 gui.mainloop()
+GPIO.cleanup()
